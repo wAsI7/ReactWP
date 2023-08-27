bf0970ea4f93354c26d23d5133a7ebeb2127a62e
Syed Wasi Ali <syedwasiali@Wasis-Mac-mini.local>
2023-08-27 20:15:47 +0530

    Test 2

--- ReactWP/src/App.jsx
+++ ReactWP/src/App.jsx
@@ -8,19 +8,21 @@ function App() {
 
   return (
     <>
+    
       <div>
-
         <img src={reactLogo} className="logo react" alt="React logo" />
         <img src={wpLogo} className="logo react" alt="WP logo" />
-        
       </div>
+
       <h1>React + WP</h1>
+
       <div className="card">
         <h4>Click on the button below to increase the value by 1</h4>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
+
     </>
   )
 }
