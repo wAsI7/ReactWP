7f9517e5fbbddefe94eaee3923f7d2766831dddc
Syed Wasi Ali <syedwasiali@Wasis-Mac-mini.local>
2023-08-27 20:10:58 +0530

    first commit

---
+++ ReactWP/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import wpLogo from './assets/wpLogo.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={wpLogo} className="logo react" alt="WP logo" />
+        
+      </div>
+      <h1>React + WP</h1>
+      <div className="card">
+        <h4>Click on the button below to increase the value by 1</h4>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
