93afde09b2f955e5b7e80ed316914c9a8aad4cc4
Syed Wasi Ali <syedwasiali@Wasis-Mac-mini.local>
2023-08-28 21:09:27 +0530

    API Call Posts From WP

--- ReactWP/src/components/Pages/Posts/index.jsx
+++ ReactWP/src/components/Pages/Posts/index.jsx
@@ -4,18 +4,26 @@ import axios from 'axios';
 
 const Posts = () => {
 
-    //const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         let url = 'https://reactwp.brandyourwork.co.in/wp-json/wp/v2/posts';
         axios.get(url).then((res) => {
-            console.log(res);
+            setPosts(res.data);
         });
         
     }, []);
-
+    console.log('Posts Out', posts);
     return (
-        <>   
+        <>
+            {
+                posts && posts.map((post) => {
+                    console.log('Posts In', posts);
+                    return (
+                        <p key={post.id} >{post.title.rendered}</p>
+                    );
+                })
+            }
         </>
     );
 }
