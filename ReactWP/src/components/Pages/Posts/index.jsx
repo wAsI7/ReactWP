6e3f4dd0e5cdc5028f3028e2eda1a5c56c2a5b64
Syed Wasi Ali <syedwasiali@Wasis-Mac-mini.local>
2023-08-28 07:35:25 +0530

    28th Aug

--- ReactWP/src/components/Pages/Posts/index.jsx
+++ ReactWP/src/components/Pages/Posts/index.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import reactLogo from '/Users/syedwasiali/Developer/React/ReactWP/src/assets/react.svg'
+import wpLogo from '/Users/syedwasiali/Developer/React/ReactWP/src/assets/wpLogo.png'
 
 
 const Posts = (propsVarName) => {
