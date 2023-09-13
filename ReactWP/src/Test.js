09a261ba7152a62c0886c7983a6e859fec36745d
Syed Wasi Ali <syedwasiali@Wasis-Mac-mini.local>
2023-09-13 22:55:31 +0530

    JS

---
+++ ReactWP/src/Test.js
@@ -0,0 +1,41 @@
+class Person {
+    constructor(name = 'Tom', age = 20, energy = 100){
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+    doSomethingFun(){
+        this.energy = this.energy - 10;
+        console.log('Energy decreasing currently at: ', this.energy);
+    }
+    sleep(){
+        this.energy = this.energy + 10;
+        console.log('Energy increasing currently at: ', this.energy);
+    }
+}
+
+class Worker extends Person {
+    constructor(name, age, energy, xp = 0, hourlyWage = 10){
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+    goToWork(){
+        this.xp = this.xp + 10;
+        console.log('Energy increasing currently at: ', this.energy);
+    }
+}
+
+function intern(){
+    var internOne = new Worker('Bob', 21, 110, 0, 10);
+    internOne.goToWork();
+    return internOne;
+}
+intern();
+
+function manager(){
+    var managerOne = new Worker('Alice', 30, 120, 100, 30);
+    managerOne.doSomethingFun();
+    return managerOne;
+}
+manager();
