8ddf66f5dde756095979dc0d5b33556912a24195
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-15 10:54:59 +0530

    JsDay3

--- ReactWP/src/Meta Front End/Test.js
+++ ReactWP/src/Meta Front End/Test.js
@@ -10,3 +10,11 @@ const car = {
 }
 
 console.log('Object is:', Object.keys(car));
+
+cosnt car2 = {
+    color: 'Blue',
+    speed: 200
+}
+
+console.log("Object Car Two is:", Object.values(car2));
+
