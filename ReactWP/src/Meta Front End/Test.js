30ac57c8b886a10e2993a620a72b8b5a857d5e03
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-19 20:32:38 +0530

    JsDay6-DataStructures

--- ReactWP/src/Meta Front End/Test.js
+++ ReactWP/src/Meta Front End/Test.js
@@ -5,3 +5,10 @@ function appendIndex(fruit, index){
 }
 
 fruits.forEach(appendIndex);
+
+const vegetables = [ 'Onion', 'Tomato', 'Potato', 'Chillies' ];
+
+vegetables.forEach(function(veggie, index){
+    console.log(`Vegetable with Index ${index} is ${veggie}`);
+});
+
