a3b92adeadfe24f8009739abe7354bd2c9ff5446
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-19 20:46:17 +0530

    JsDay6-DataStructures

--- ReactWP/src/Meta Front End/Test.js
+++ ReactWP/src/Meta Front End/Test.js
@@ -1,3 +1,5 @@
+/*Data Structures - Arrays */
+
 const fruits = [ 'Apple', 'Banana', 'Custard', 'Orange', 'Pineapple', 'Watermelon' ];
 
 function appendIndex(fruit, index){
@@ -18,3 +20,23 @@ vegetables.forEach(function(veggie, index){
 vegetables.forEach((veggie, index) => {
     console.log(`Vegetable with Index ${index} is ${veggie}`);    
 });
+
+
+/*Data Structures - Objects */
+
+var car = {
+    color: 'Red',
+    speed: 120,
+    mileage: 25,
+    brand: 'Honda'
+};
+
+// Accessing properties
+console.log(`The Color of the Car is: ${car.color}`);
+
+// Modifying properties
+car.color = 'Blue';
+console.log(`The Color of the Car is: ${car.color}`);
+
+// Adding new properties
+// Iterating through an object
