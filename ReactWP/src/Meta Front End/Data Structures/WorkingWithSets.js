dc464a944d65973706cd559c9d733c9bc0acaa6f
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-21 20:59:53 +0530

    JsDay7 - Working With Arrays

---
+++ ReactWP/src/Meta Front End/Data Structures/WorkingWithSets.js
@@ -0,0 +1,7 @@
+const var1 = ['Apple', 'Banana', 'apple', 'Watermelon', 'Apple', 'Grapes'];
+
+const var2 = new Set(var1);
+
+console.log(var2);
+
+//As we tried the Set method by changing the value of apple in lowercase it is case sensitive and takes the value as sepratete entity
