5aa4ba967bbcb756be80766f534b10d35d42fd5c
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-21 20:53:53 +0530

    JsDay7 - Working With Arrays

---
+++ ReactWP/src/Meta Front End/Data Structures/WorkingWithMaps.js
@@ -0,0 +1,11 @@
+var name = new Map();
+
+name.set(1, 'Syed');
+name.set(2, 'Wasi');
+name.set(3, 'Ali');
+
+console.log(name);
+
+//Let's Try fetching the mapped value from the key
+
+console.log(name.get(2));
