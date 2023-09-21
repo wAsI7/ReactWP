2e913d5f63ca2c24ae18accd559e0ea9a2244c1e
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-21 20:48:31 +0530

    JsDay7 - Working With Arrays

---
+++ ReactWP/src/Meta Front End/Data Structures/WorkingWithObjects.js
@@ -0,0 +1,16 @@
+const arr = [];
+const drone = {
+    speed: 200,
+    color: 'Red'
+}
+
+const droneKeys = Object.keys(drone);
+droneKeys.forEach(function(key){
+    arr.push(key, drone[key]);
+})
+
+console.log(`Values fetched in the array from drone object is ${arr}`);
+
+for(var i=0; i<4; i++){
+    console.log(typeof(arr[i]));
+}
