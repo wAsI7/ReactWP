173f179f6bac341aa87d1f15de492caab438215e
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-21 21:35:38 +0530

    JsDay7 - WorkingWithSpreadRestOperators

--- ReactWP/src/Meta Front End/Programming JS/Data Structure/SpreadAndRest.js
+++ ReactWP/src/Meta Front End/Programming JS/Data Structure/SpreadAndRest.js
@@ -1,6 +1,6 @@
-//Spread Operator
-const wadrobe = ['TShirt', 'Jeans', 'Pants', 'Shirts', 200, 'Shoes'];
+const wadrobe = ['TShirt', 'Jeans', 'Pants', 'Shirts', 200, 300, 120, 430];
 
+//Spread Operator
 const ready = (...wadrobe) => {
     for(var value of wadrobe){
         console.log(`${value} is of type ${typeof(value)}`);        
@@ -8,6 +8,18 @@ const ready = (...wadrobe) => {
     
 }
 
-ready(...wadrobe);
+//ready(...wadrobe);
 
 //Rest Operator
+const addTax = (taxRate, ...itemsBought) => {
+    return itemsBought.map(item => (item * taxRate)/100);    
+}
+
+var taxAndPrice = [2.5, 300, 240, 80, 90];
+
+const taxOfEach = addTax(...taxAndPrice);
+
+var priceWithTax = [];
+for(var val of )
+
+console.log(shoppingCart);
