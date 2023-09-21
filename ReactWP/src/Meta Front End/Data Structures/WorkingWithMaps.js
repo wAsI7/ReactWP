d2889329ca34af05e36aa77763b75aa9982567d4
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-21 20:55:24 +0530

    JsDay7 - Working With Arrays

--- ReactWP/src/Meta Front End/Data Structures/WorkingWithMaps.js
+++ ReactWP/src/Meta Front End/Data Structures/WorkingWithMaps.js
@@ -9,3 +9,7 @@ console.log(name);
 //Let's Try fetching the mapped value from the key
 
 console.log(name.get(2));
+
+//In Map method '.set' is used to assign value to the given key
+//And '.get' is used to get the value assigned to the key
+
