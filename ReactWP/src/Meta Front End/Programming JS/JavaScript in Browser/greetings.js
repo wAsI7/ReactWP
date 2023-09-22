244f87441b15c1b073630e76fbbb7c47ce5282f5
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-22 05:49:45 +0000

    JsDay9 - Js Modules

---
+++ ReactWP/src/Meta Front End/Programming JS/JavaScript in Browser/greetings.js
@@ -0,0 +1,13 @@
+export const informalGreeting = (person) => {
+    console.log(`Hey there!! ${person}`);
+}
+
+export const formalGreeting = (person) => {
+    console.log(`Good Morning  ${person}`);
+}
+
+const greeting = () => {
+    console.log('HELLO');
+}
+
+export default greeting;
